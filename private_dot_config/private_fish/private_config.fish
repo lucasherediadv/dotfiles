d982b9c8252880cdf9e5e6ee2817912ea41bbdda
Lucas Heredia <lucasherediadv@gmail.com>
2026-02-08 23:48:46 +0000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -1,22 +1,20 @@
 set -g fish_greeting
 
-# Editor
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-# Pager
 set -gx PAGER less
 set -gx LESSHISTFILE /dev/null
 
 if status is-interactive
-    fish_vi_key_bindings
-    function fish_mode_prompt
-    end
+  fish_vi_key_bindings
+  function fish_mode_prompt
+  end
 
-    abbr vi nvim
-    abbr c clear -x
-    abbr clear clear -x
-    abbr ls ls -hF --color=auto --group-directories-first
-    abbr la ls -hF --color=auto --group-directories-first --all
-    abbr ll ls -hF --color=auto --group-directories-first -l --all
+  abbr vi nvim
+  abbr c clear -x
+  abbr clear clear -x
+  abbr ls ls -Fh --color --group-directories-first
+  abbr la ls -aFh --color --group-directories-first
+  abbr ll ls -aFhl --color --group-directories-first
 end

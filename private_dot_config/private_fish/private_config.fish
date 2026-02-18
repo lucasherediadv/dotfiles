e6a48744e27d20d5405f49922f7080d673917848
Lucas Heredia <lucasherediadv@gmail.com>
2026-02-18 16:12:59 +0000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -4,11 +4,11 @@ set -gx PAGER less
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-set -gx REPOS "$HOME/repos"
-set -gx GITUSER "lucasherediadv"
-set -gx GHREPOS "$REPOS/github.com/$GITUSER"
+set -gx REPOS $HOME/repos
+set -gx GITUSER lucasherediadv
+set -gx GHREPOS $REPOS/github.com/$GITUSER
 
-set -gx CDPATH ".:$GHREPOS:$REPOS/github.com:$HOME"
+set -gx CDPATH .:$GHREPOS:$REPOS/github.com:$HOME
 
 if status is-interactive
   fish_vi_key_bindings
@@ -17,12 +17,11 @@ if status is-interactive
 
   abbr vi nvim
   abbr c clear -x
+  abbr free free -h -m
   abbr clear clear -x
+  abbr tree tree -a -C
   abbr todo $EDITOR $HOME/.todo.md
-  abbr cat bat --theme-dark=gruvbox-dark
-  abbr ls eza -F --icons --group-directories-first
-  abbr la eza -F --icons --group-directories-first -a
-  abbr ll eza -F --icons --group-directories-first -a -l
-
-  starship init fish | source
+  abbr ls ls -F --group-directories-first
+  abbr la ls -F --group-directories-first -a
+  abbr ll ls -F --group-directories-first -a -l
 end

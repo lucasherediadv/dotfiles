7ea65256113f0554c247675f5f1a1e31c1e0e0f0
Lucas Heredia <lucasherediadv@gmail.com>
2026-02-20 18:26:46 +0000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/private_config.fish
+++ private_dot_config/private_fish/private_config.fish
@@ -1,6 +1,8 @@
 set -g fish_greeting
 
 set -gx PAGER less
+set -gx LESSHISTFILE /dev/null
+
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
@@ -8,29 +10,48 @@ set -gx REPOS $HOME/repos
 set -gx GITUSER lucasherediadv
 set -gx GHREPOS $REPOS/github.com/$GITUSER
 
+set -gx SCRIPTS $GHREPOS/scripts
+set -gx DOTFILES $GHREPOS/dotfiles
+
 set -gx CDPATH .:$GHREPOS:$REPOS/github.com:$HOME
 
 if status is-interactive
-  fish_vi_key_bindings
-  function fish_mode_prompt
-  end
+    fish_vi_key_bindings
 
-  abbr vi nvim
-  abbr p podman
-  abbr df df -h
-  abbr du du -h
-  abbr cz chezmoi
-  abbr c clear -x
-  abbr clear clear -x
-  abbr free free -h -m
-  abbr ip ip --color=auto
-  abbr pstree pstree -UC age
-  abbr diff diff --color=auto
-  abbr grep grep --color=auto
-  abbr todo $EDITOR $HOME/.todo.md
-  abbr ls ls -F --group-directories-first
-  abbr tree tree -a -C -I .git --gitignore
-  abbr psc ps xawf -eo pid,user,cgroup,args
-  abbr la ls -F --group-directories-first -a
-  abbr ll ls -F --group-directories-first -a -l
+    abbr vi nvim
+    abbr df df -h
+    abbr du du -h
+    abbr cz chezmoi
+    abbr c clear -x
+    abbr clear clear -x
+    abbr free free -h -m
+    abbr ip ip --color=auto
+    abbr pstree pstree -UC age
+    abbr diff diff --color=auto
+    abbr grep grep --color=auto
+    abbr psc ps xawf -eo pid,user,cgroup,args
+    abbr ls eza --icons --group-directories-first -F
+    abbr la eza --icons --group-directories-first -aF
+    abbr ll eza --icons --group-directories-first -alF
+    abbr tree eza --icons --group-directories-first --tree --ignore-glob=.git -aF
+
+    abbr p podman
+    abbr pc podman rm --all
+    abbr pps podman ps --all
+    abbr pi podman images --all
+    abbr todo $EDITOR $HOME/.todo.md
+    abbr to bat --theme-dark=gruvbox-dark $HOME/.todo.md
+
+    abbr me cd $GHREPOS
+    abbr dot cd $DOTFILES
+    abbr scripts cd $SCRIPTS/bin
+    abbr ghrepos cd $REPOS/github.com
+
+    abbr lg lazygit
+    abbr gs git status
+    abbr gl git log --graph --decorate --oneline
+
+    abbr b bat --theme-dark=gruvbox-dark
+
+    starship init fish | source
 end

1bb59f42c970f7b88fadce7ab2d91c6e524a5946
Lucas Agustin Heredia <lucasherediadv@gmail.com>
2026-08-21 12:54:30 -0300

    feat: update nvim configuration

---
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "material"
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
